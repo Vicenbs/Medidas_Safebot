2d9cfe19db85f658e7ff09a0e0142f5567080d9a
Vicente Barreiro <126165516+Vicenbs@users.noreply.github.com>
2024-04-24 11:43:52 +0200

    Update yolov8.py

--- yolov8.py
+++ yolov8.py
@@ -1,17 +1,3 @@
-# Tencent is pleased to support the open source community by making ncnn available.
-#
-# Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
-#
-# Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-# in compliance with the License. You may obtain a copy of the License at
-#
-# https://opensource.org/licenses/BSD-3-Clause
-#
-# Unless required by applicable law or agreed to in writing, software distributed
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-
 import sys
 import cv2
 from ncnn.model_zoo import get_model
@@ -45,7 +31,7 @@ if __name__ == "__main__":
         draw_detection_objects(m, net.class_names, objects)
 
         for obj in objects:
-            datos = datos.append({"image_name": image_name, "class": net.class_names[int(obj.label)], "confidence": obj.prob*100, "tiempo_inferencia": elapsed}, ignore_index=True)
+            datos = datos.loc[len(datos)]={"image_name": image_name, "class": net.class_names[int(obj.label)], "confidence": obj.prob*100, "tiempo_inferencia": elapsed}
 
         cv2.imwrite("resultados/"+image_name+"_results.png", m)
 
