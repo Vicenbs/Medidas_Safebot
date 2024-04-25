a854713ea29588babbc3d1d9059ad85402b70bf0
Vicente Barreiro <126165516+Vicenbs@users.noreply.github.com>
2024-04-25 09:48:21 +0200

    Update yolov4.py

--- yolov4.py
+++ yolov4.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     image_folder = "images"
     image_files = os.listdir(image_folder)
     net = get_model(
-            "yolov4"
+            "yolov4",
+        use_gpu = False
         )
     datos = pd.DataFrame(columns=["image_name", "class", "confidence", "tiempo_inferencia"])
 
